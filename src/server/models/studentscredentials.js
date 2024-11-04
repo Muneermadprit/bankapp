b8bcb4ca2574e7bc06c9b500f42b3a2cdda72f9c
Muneer A <137588707+Muneermadprit@users.noreply.github.com>
2024-11-04 19:20:40 +0530

    Update studentscredentials.js

--- src/server/models/studentscredentials.js
+++ src/server/models/studentscredentials.js
@@ -1,17 +1,48 @@
-class Students {
-    constructor(name, standard,id,devision,fathername,classteacher) {
-        
-        this.name = name;
-        this.standard = standard;
-        this.id = id,
-        this.devision = devision,
-        this.fathername = fathername;
-        this.classteacher = classteacher;
-        
-     
+const { DataTypes } = require('sequelize');
+const sequelize = require('./index'); // Sequelize instance
 
+const StudentCredential = sequelize.define('studentscredentials', {
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+    },
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    phonenumber: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+            isNumeric: true,
+            len: [10, 15], // Adjust based on expected phone number length
+        },
+    },
+    parentid: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        unique: true,
+    },
+    fathername: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    devision: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    standard: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    classteacher: {
+        type: DataTypes.STRING,
+        allowNull: true, // Assuming this could be optional
+    },
+}, {
+    tableName: 'studentscredentials',
+    timestamps: false, // Disable createdAt and updatedAt fields
+});
 
-    }
-}
-
-module.exports = Students;
+module.exports = StudentCredential;
