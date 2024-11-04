8c5e49c31d193aa675a413678c326449f61e5f65
Muneer A <137588707+Muneermadprit@users.noreply.github.com>
2024-11-04 19:15:43 +0530

    Update acadamics.js

--- src/server/models/acadamics.js
+++ src/server/models/acadamics.js
@@ -1,15 +1,59 @@
-class Acadamics{
-    constructor(socialscience,mathematics,english,IT){
-        this.socialscience = socialscience;
-        this.mathematics = mathematics;
-        this.english = english;
-        this.IT = IT;
-      
+const { DataTypes } = require('sequelize');
+const sequelize = require('./index'); // Assuming this is the initialized Sequelize instance
 
-
-       
+const Acadamics = sequelize.define('Acadamics', {
+    student_id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    Math: {
+        type: DataTypes.FLOAT,
+        allowNull: true // Assuming grades can be null if not available
+    },
+    Science: {
+        type: DataTypes.FLOAT,
+        allowNull: true
+    },
+    English: {
+        type: DataTypes.FLOAT,
+        allowNull: true
+    },
+    History: {
+        type: DataTypes.FLOAT,
+        allowNull: true
+    },
+    Geography: {
+        type: DataTypes.FLOAT,
+        allowNull: true
+    },
+    Physics: {
+        type: DataTypes.FLOAT,
+        allowNull: true
+    },
+    Chemistry: {
+        type: DataTypes.FLOAT,
+        allowNull: true
+    },
+    Computer_Science: {
+        type: DataTypes.FLOAT,
+        allowNull: true
+    },
+    Physical_Education: {
+        type: DataTypes.FLOAT,
+        allowNull: true
+    },
+    Arts: {
+        type: DataTypes.FLOAT,
+        allowNull: true
     }
-
-}
+}, {
+    tableName: 'students', // Specify table name if different from model name
+    timestamps: false // Assuming you don’t need createdAt and updatedAt fields
+});
 
 module.exports = Acadamics;
