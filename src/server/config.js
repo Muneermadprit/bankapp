0c63636e31d7c248c6fab7dda8dab8a596d48a18
Muneer A <137588707+Muneermadprit@users.noreply.github.com>
2024-11-04 19:25:33 +0530

    Update config.js

--- src/server/config.js
+++ src/server/config.js
@@ -6,30 +6,31 @@ const {
     PORT,
     HOST,
     HOST_URL,
-    API_KEY,
-    AUTH_DOMAIN,
-    PROJECT_ID,
-    STORAGE_BUCKET,
-    MESSAGING_SENDER_ID,
-    APP_ID,
-    MEASUREMENT_ID
+
+    // MySQL Configuration
+    MYSQL_HOST,
+    MYSQL_USER,
+    MYSQL_PASSWORD,
+    MYSQL_DB_NAME
 } = process.env;
 
 assert(PORT, 'PORT is required');
 assert(HOST, 'HOST is required');
 
+// Assert MySQL configuration
+assert(MYSQL_HOST, 'MYSQL_HOST is required');
+assert(MYSQL_USER, 'MYSQL_USER is required');
+assert(MYSQL_PASSWORD, 'MYSQL_PASSWORD is required');
+assert(MYSQL_DB_NAME, 'MYSQL_DB_NAME is required');
+
 module.exports = {
     port: PORT,
     host: HOST,
     url: HOST_URL,
-    firebaseConfig: {
-        apiKey: API_KEY,
-        authDomain: AUTH_DOMAIN,
-        projectId: PROJECT_ID,
-        storageBucket: STORAGE_BUCKET,
-        messagingSenderId: MESSAGING_SENDER_ID,
-        appId: APP_ID,
-        measurementId: MEASUREMENT_ID
+    mysqlConfig: {
+        host: MYSQL_HOST,
+        user: MYSQL_USER,
+        password: MYSQL_PASSWORD,
+        database: MYSQL_DB_NAME,
     }
 };
-
