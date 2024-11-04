2b223933775163e0a7c6bec22ab0c11fe3ab8529
Muneer A <137588707+Muneermadprit@users.noreply.github.com>
2024-11-04 19:13:22 +0530

    Update db.js

--- src/server/db.js
+++ src/server/db.js
@@ -1,16 +1,28 @@
-const { initializeApp } = require('firebase/app');
-const { getFirestore } = require('firebase/firestore');
-const config = require('./config');
+const mysql = require('mysql2');
+const config = require('./config'); // Ensure this file has your MySQL config
 
-// Initialize Firebase
-const app = initializeApp(config.firebaseConfig);
-console.log('Firebase app name:', app.name);
+// Create a MySQL connection
+const connection = mysql.createConnection({
+  host: config.mysqlConfig.host,
+  user: config.mysqlConfig.user,
+  password: config.mysqlConfig.password,
+  database: config.mysqlConfig.database,
+});
+
+// Connect to MySQL
+connection.connect((err) => {
+  if (err) {
+    console.error('Error connecting to MySQL:', err);
+    return;
+  }
+  console.log('Connected to MySQL as id', connection.threadId);
+});
+
+// Export the connection for use in other modules
+module.exports = connection;
 
 
-// Get a Firestore instance
-const db = getFirestore(app);
 
-module.exports = db;
 
 
 
