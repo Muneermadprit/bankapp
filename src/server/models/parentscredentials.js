81a18cd1133ddf5f570460b59ef1d1bf71bf7c41
Muneer A <137588707+Muneermadprit@users.noreply.github.com>
2024-11-04 19:19:53 +0530

    Update parentscredentials.js

--- src/server/models/parentscredentials.js
+++ src/server/models/parentscredentials.js
@@ -1,10 +1,45 @@
-class Parentscredentials {
-    constructor(name,email,password) {
-        this.name = name;
-        this.email = email,
-        this.password = password
-       
-    }
-}
+const { DataTypes } = require('sequelize');
+const sequelize = require('./index'); // Sequelize instance
+
+const Parentscredentials = sequelize.define('parentcredential', {
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true, // Automatically incrementing value
+        primaryKey: true,    // Set as primary key
+        allowNull: false,    // Cannot be null
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+            isEmail: true,
+        },
+    },
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    parentid: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        unique: true,
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    phone: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+            isNumeric: true,
+            len: [10, 15] // Assuming phone numbers are between 10 to 15 characters long
+        },
+    },
+}, {
+    tableName: 'parentcredential',
+    timestamps: true, // Adds createdAt and updatedAt fields
+});
 
 module.exports = Parentscredentials;
+
