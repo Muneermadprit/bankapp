f9c350b02ab05942076184f2f0a4e19596a1e4ad
Muneer A <137588707+Muneermadprit@users.noreply.github.com>
2024-11-05 17:09:41 +0530

    Update Textmessage.js

--- src/server/Textmessage.js
+++ src/server/Textmessage.js
@@ -1,1 +1,17 @@
+function createTextMessage(contact, message) {
+    return {
+        messaging_product: "whatsapp",
+        recipient_type: "individual",
+        to: contact,
+        type: "text",
+        text: {
+            preview_url: false,
+            body: message
+        }
+    };
+}
 
+// Example usage:
+module.exports = {
+    createTextMessage
+}
