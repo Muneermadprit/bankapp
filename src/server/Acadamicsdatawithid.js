d81cb0e4a4a73e8e408a411e64a68be5b1c566e4
Muneer A <137588707+Muneermadprit@users.noreply.github.com>
2024-11-19 21:42:56 +0530

    Update Acadamicsdatawithid.js

--- src/server/Acadamicsdatawithid.js
+++ src/server/Acadamicsdatawithid.js
@@ -1,11 +1,17 @@
 const cache = require('./cache');
 const Acadamicsdata = require('./models/acadamics');
 
-const { sendMessage } = require('./sendingtowhatsapp');
 const { createTextMessage } = require('./Textmessage');
+const {generateRequest} = require('./sendresponse');
+const {sendMessage} = require('./sendingtowhatsapp');
 
 // Function to get attendance records by student ID
 const Acadamics = async (req, res, id, user) => {
+   let headerText
+   let bodyText
+   let footerText
+   let  buttonTitle
+
     // Destructure studentId from the request body
     const userdata = cache.get(user);
     const userid = userdata.id;
@@ -36,19 +42,35 @@ const Acadamics = async (req, res, id, user) => {
             PHYSICAL EDUCATION: ${Acadamics.Physical_Education}\n
             ARTS: ${Acadamics.Arts}\n
             ---------------------------`
-            
 
 
-  
-    
+
+
+
 
         // Create WhatsApp message and send it
         const whatsappTextMessage = createTextMessage(userid, whatsappResponse);
         await sendMessage(whatsappTextMessage); // Ensure sendMessage is awaited for proper handling
 
         // Optionally, you might want to send a response back to the client
-        return res.status(200).json({ message: 'Attendance record sent via WhatsApp.' });
-        
+        //return res.status(200).json({ message: 'Attendance record sent via WhatsApp.' });
+
+        headerText = 'Return Back To Mainmenu';
+       bodyText = "Discover What's Next – Click Here to Go for More";
+       footerText = "";
+       buttonTitle = "";
+
+
+
+       const menuList =[{
+        id:'Back to mainmenu',
+        title: 'Back to Mainmenu',
+        description:'Return Back',
+
+    }] 
+      const backtomenu = generateRequest(user, headerText, bodyText, footerText, buttonTitle,menuList) 
+      sendMessage(backtomenu)
+
     } catch (error) {
         console.error('Error fetching attendance data:', error);
         res.status(500).json({ error: 'Error fetching attendance data' }); // Send error response
